906a9371eea5680be094aaeff49033d654965d29
Lucio <lucioborelli94@gmail.com>
2026-05-10 15:33:42 +0200

    Aggiungere i file di progetto.

---
+++ es 1.13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+Thread t1 = new Thread(() =>
+{
+    Thread.Sleep(3000);
+});
+
+Thread t2 = new Thread(() =>
+{
+    Thread.Sleep(4000);
+});
+
+Thread t3 = new Thread(() =>
+{
+    Thread.Sleep(5000);
+});
+
+t1.Start();
+t2.Start();
+t3.Start();
+t1.Join();
+t2.Join();
+t3.Join();
+
+stopwatch.Stop();
+Console.WriteLine("Totale tempo di esecuzione: ");
+Console.WriteLine($" {stopwatch.ElapsedMilliseconds} ms");
+Console.WriteLine($"{stopwatch.Elapsed.TotalSeconds} secondi");
